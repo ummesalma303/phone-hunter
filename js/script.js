028a768d3b8bd8c3d7190c8031c4bd895690ee02
ummesalma303 <ummesalma333999@gmail.com>
2024-10-04 20:08:48 -0700

    added modal

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 const loadAllPhones = async (status,brandName) => {
-  const spinner = document.getElementById("spinner");
-    spinner.style.display = "none";
+//   const spinner = document.getElementById("spinner");
+//     spinner.style.display = "none";
 
     const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=${brandName?brandName:'iphone'}`);
     const data = await response.json();
-console.log(data);
     if (status) {
          displayPhones(data.data);
     } else {
@@ -20,7 +19,7 @@ const handleSearch = () => {
     
   setTimeout(() => {
     loadAllPhones(false,searchBox);
-  }, 3000);
+  }, 5000);
 };
 
 const handleShowAll = () => {
@@ -29,6 +28,8 @@ const handleShowAll = () => {
 
 const displayPhones = (phones) => {
     const phonesContainer = document.getElementById("phones-container");
+    document.getElementById("phones-container").innerHTML = "";
+
     phones.forEach(phone => {
         const { brand, phone_name,slug, image } = phone;
        const div = document.createElement('div')
@@ -45,13 +46,49 @@ const displayPhones = (phones) => {
     <h2 class="card-title">${brand}</h2>
     <p>${phone_name}</p>
     <div class="card-actions">
-      <button class="btn btn-primary">Show Details</button>
+      <button onclick="phoneDetails('${slug}')" class="btn btn-primary">Show Details</button>
     </div>
   </div>
 </div>
        `;
-console.log(phone);
+        
        phonesContainer.appendChild(div)
    });
+}
+
+const phoneDetails = async (slug) => {
+     const response = await fetch(
+       ` https://openapi.programming-hero.com/api/phone/${slug}`
+     );
+    const data = await response.json();
+    showDetails(data.data)
+}
+const showDetails = (phone) => {
+
+
+
+console.log(phone);
+const { releaseDate, name, brand, image } = phone;
+const modalContainer = document.getElementById("modal-container");
+
+modalContainer.innerHTML = `
+    <dialog id="my_modal_1" class="modal">
+  <div class="modal-box flex flex-col justify-center items-center">
+  <img src=${image}>
+    <h3 class="text-lg font-bold">${brand}</h3>
+    <p class="py-4">${name}</p>
+    <p>${releaseDate}</p>
+    <div class="modal-action">
+      <form method="dialog">
+        
+        <!-- if there is a button in form, it will close the modal -->
+        <button class="btn">Close</button>
+      </form>
+    </div>
+  </div>
+</dialog>
+    `;
+
+my_modal_1.showModal();
 }
 loadAllPhones(false,'iphone');
